4ac272a518e5b87ecf1b60257136d5b85dddf687
luis <luishsilva09@gmail.com>
2022-04-15 17:03:38 -0300

    enviar mensagens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let nomeUsuario = {}
 
-let idInterval = setInterval(statusUsuario, 5000)
+let idInterval = setInterval(statusUsuario, 4000)
 
 function entrarSala() {
     nomeUsuario = { name: prompt("Digite seu nome:") };
@@ -37,32 +37,42 @@ function mensagens(response) {
     let dados = response.data
 
     let mensagens = document.querySelector(".container");
+
     mensagens.innerHTML = ''
     for (let i = 0; i < dados.length; i++) {
-
+        if (dados[i].type === "message") {
+            mensagens.innerHTML +=
+                `<div class="todos">(${dados[i].time}) ${dados[i].from} ${dados[i].text}</div>`
+        }
         if (dados[i].type === "status") {
             mensagens.innerHTML +=
                 `<div class="status">(${dados[i].time}) ${dados[i].from} ${dados[i].text}</div>`
-        }
-        if (dados[i].type === "message") {
-            `<div class="todos">(${dados[i].time}) ${dados[i].from} ${dados[i].text}</div>`
-        }
-        if (dados[i].type === "private_message") {
-            `<div class="reservada">(${dados[i].time}) ${dados[i].from} ${dados[i].text}</div>`
-        }
-    }
-    mensagens.scrollIntoView(false)
-}
-function enviarMensagem() {
-    let texto = document.querySelector(".mensagem").value
-    let mensagem = {
-        from: nomeUsuario,
-        to: '',
-        text: texto,
-        type:"message"
-    }
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem)
 
-}
+        }
+        if (dados[i].type === "private_message" && dados[i].to === nomeUsuario.name) {
+            mensagens.innerHTML +=
+                `<div class="reservadas">(${dados[i].time}) ${dados[i].from} ${dados[i].text}</div>`
+
+        }
+        mensagens.scrollIntoView(false)
+
+    }}
+
+    function enviarMensagem() {
+        let texto = document.querySelector(".mensagem").value
+        let mensagem = {
+            'from': nomeUsuario.name,
+            'to': "Todos",
+            'text': texto,
+            'type': "message"
+        }
+        console.log(mensagem)
+        const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem)
+        promise.then(function(){
+            document.querySelector(".mensagem").value = ''
+        });
+        
+    }
+
 entrarSala()
 buscarMensagens()
